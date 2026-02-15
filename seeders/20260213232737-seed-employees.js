1d431d0efd660bc365590bfe6abe522eff0d869d
Arsyad Irfan Fauza <68624326+irzaricky@users.noreply.github.com>
2026-02-15 21:26:18 +0700

    fix: penambahan data seed user

--- seeders/20260213232737-seed-employees.js
+++ seeders/20260213232737-seed-employees.js
@@ -39,16 +39,177 @@ export default {
     };
 
     const rawData = [
-      { full_name: 'Andi Pratama', emp_no: 'EMP-001', factory: 'Factory Assy', line: 'Line Assembly Frame', phone: '8123456001', email: 'andi.pratama@company.com', username: 'andi.pratama', role: 'Staff PPIC' },
-      { full_name: 'Budi Santoso', emp_no: 'EMP-002', factory: 'Factory Assy', line: 'Line Assembly Electrical', phone: '8123456002', email: 'budi.santoso@company.com', username: 'budi.santoso', role: 'Supervisor PPIC' },
-      { full_name: 'Citra Lestari', emp_no: 'EMP-003', factory: 'Factory Painting', line: 'Line Painting Primer', phone: '8123456003', email: 'citra.lestari@company.com', username: 'citra.lestari', role: 'Admin Production' },
-      { full_name: 'Dewi Anggraini', emp_no: 'EMP-004', factory: 'Factory Painting', line: 'Line Painting Color Coat', phone: '8123456004', email: 'dewi.anggraini@company.com', username: 'dewi.anggraini', role: 'Staff PPIC' },
-      { full_name: 'Eko Nugroho', emp_no: 'EMP-005', factory: 'Factory Warehouse', line: 'Line Packing', phone: '8123456005', email: 'eko.nugroho@company.com', username: 'eko.nugroho', role: 'Warehouse Staff' },
-      { full_name: 'Fajar Hidayat', emp_no: 'EMP-006', factory: 'Factory Warehouse', line: 'Line QC Final', phone: '8123456006', email: 'fajar.hidayat@company.com', username: 'fajar.hidayat', role: 'Warehouse Leader' },
-      { full_name: 'Rina Wulandari', emp_no: 'EMP-007', factory: 'Factory Assy', line: 'Line Assembly Final', phone: '8123456007', email: 'rina.wulandari@company.com', username: 'rina.wulandari', role: 'Supervisor PPIC' },
-      { full_name: 'Umar Winarno', emp_no: 'EMP-008', factory: null, line: null, phone: '8123456008', email: 'umar.winarno@company.com', username: 'umar.winarno', role: 'Driver' },
-      { full_name: 'Harsanto Anggriawan', emp_no: 'EMP-009', factory: null, line: null, phone: '8123456009', email: 'harsanto@company.com', username: 'harsanto.ang', role: 'Driver' },
-      { full_name: 'Yuliana Lailasari', emp_no: 'EMP-010', factory: null, line: null, phone: '8123456010', email: 'yuliana@company.com', username: 'yuliana.laila', role: 'Driver' }
+      { 
+        full_name: 'Andi Pratama', 
+        emp_no: 'EMP-001', 
+        factory: 'Factory Assy', 
+        line: 'Line Assembly Frame', 
+        phone: '8123456001', 
+        email: 'andi.pratama@company.com', 
+        username: 'andi.pratama', 
+        role: 'Superadmin' 
+      },
+      { 
+        full_name: 'Budi Santoso', 
+        emp_no: 'EMP-002', 
+        factory: 'Factory Assy', 
+        line: 'Line Assembly Electrical', 
+        phone: '8123456002', 
+        email: 'budi.santoso@company.com', 
+        username: 'budi.santoso', 
+        role: 'Staff Sales Forecast' 
+      },
+      { 
+        full_name: 'Citra Lestari', 
+        emp_no: 'EMP-003', 
+        factory: 'Factory Painting', 
+        line: 'Line Painting Primer', 
+        phone: '8123456003', 
+        email: 'citra.lestari@company.com', 
+        username: 'citra.lestari', 
+        role: 'Supervisor Sales Forecast' 
+      },
+      { 
+        full_name: 'Dewi Anggraini', 
+        emp_no: 'EMP-004', 
+        factory: 'Factory Painting', 
+        line: 'Line Painting Color Coat', 
+        phone: '8123456004', 
+        email: 'dewi.anggraini@company.com', 
+        username: 'dewi.anggraini', 
+        role: 'Staff Sales Order' 
+      },
+      { 
+        full_name: 'Eko Nugroho', 
+        emp_no: 'EMP-005', 
+        factory: 'Factory Warehouse', 
+        line: 'Line Packing', 
+        phone: '8123456005', 
+        email: 'eko.nugroho@company.com', 
+        username: 'eko.nugroho', 
+        role: 'Supervisor Sales Order' 
+      },
+      { 
+        full_name: 'Fajar Hidayat', 
+        emp_no: 'EMP-006', 
+        factory: 'Factory Warehouse', 
+        line: 'Line QC Final', 
+        phone: '8123456006', 
+        email: 'fajar.hidayat@company.com', 
+        username: 'fajar.hidayat', 
+        role: 'Staff Sales Delivery' 
+      },
+      { 
+        full_name: 'Rina Wulandari', 
+        emp_no: 'EMP-007', 
+        factory: 'Factory Assy', 
+        line: 'Line Assembly Final', 
+        phone: '8123456007', 
+        email: 'rina.wulandari@company.com', 
+        username: 'rina.wulandari', 
+        role: 'Supervisor Sales Delivery' 
+      },
+      { 
+        full_name: 'Umar Winarno', 
+        emp_no: 'EMP-008', 
+        factory: null, 
+        line: null, 
+        phone: '8123456008', 
+        email: 'umar.winarno@company.com', 
+        username: 'umar.winarno', 
+        role: 'Admin sales' 
+      },
+      { 
+        full_name: 'Harsanto Anggriawan', 
+        emp_no: 'EMP-009', 
+        factory: null, 
+        line: null, 
+        phone: '8123456009', 
+        email: 'harsanto@company.com', 
+        username: 'harsanto.ang', 
+        role: 'Warehouse Staff' 
+      },
+      { 
+        full_name: 'Yuliana Lailasari', 
+        emp_no: 'EMP-010', 
+        factory: null, 
+        line: null, 
+        phone: '8123456010', 
+        email: 'yuliana@company.com', 
+        username: 'yuliana.laila', 
+        role: 'Supervisor Warehouse' 
+      },
+      { 
+        full_name: 'Cakrajiya Najmudin', 
+        emp_no: 'EMP-011', 
+        factory: 'Factory Warehouse', 
+        line: 'Line Packing', 
+        phone: '8123456011', 
+        email: 'cakrajiya.najmudin@company.com', 
+        username: 'cakrajiya.najmudin', 
+        role: 'Admin Warehouse' 
+      },
+      { 
+        full_name: 'Taufik Hidayat', 
+        emp_no: 'EMP-012', 
+        factory: 'Factory Assy', 
+        line: 'Line Assembly Frame', 
+        phone: '8123456012', 
+        email: 'taufik.hidayat@company.com', 
+        username: 'taufik.hidayat', 
+        role: 'Admin PPIC' 
+      },
+      { 
+        full_name: 'Ridwan Utama', 
+        emp_no: 'EMP-013', 
+        factory: 'Factory Assy', 
+        line: 'Line Assembly Electrical', 
+        phone: '8123456013', 
+        email: 'ridwan.utama@company.com', 
+        username: 'ridwan.utama', 
+        role: 'Staff PPIC' 
+      },
+      { 
+        full_name: 'Rahmi Wastuti', 
+        emp_no: 'EMP-014', 
+        factory: 'Factory Assy', 
+        line: 'Line Assembly Final', 
+        phone: '8123456014', 
+        email: 'rahmi.wastuti@company.com', 
+        username: 'rahmi.wastuti', 
+        role: 'Supervisor PPIC' 
+      },
+      { 
+        full_name: 'Lukman Mandala', 
+        emp_no: 'EMP-015', 
+        factory: 'Factory Painting', 
+        line: 'Line Painting Primer', 
+        phone: '8123456015', 
+        email: 'lukman.mandala@company.com', 
+        username: 'lukman.mandala', 
+        role: 'Admin Production' 
+      },
+      { 
+        full_name: 'Aswani Wahyudin', 
+        emp_no: 'EMP-016', 
+        factory: null, 
+        line: null, 
+        phone: '8123456016', 
+        email: 'aswani.wahyudin@company.com', 
+        username: 'aswani.wahyudin', 
+        role: 'Purchasing Manager' 
+      },
+      { 
+        full_name: 'Bahuwirya Simbolon', 
+        emp_no: 'EMP-017', 
+        factory: null, 
+        line: null, 
+        phone: '8123456017', 
+        email: 'bahuwirya.simbolon@company.com', 
+        username: 'bahuwirya.simbolon', 
+        role: 'Driver' 
+      },
+      
     ];
 
     const users = [];

779eb1ca39ecdf0a2c56e1b2bc5b456da8fc4532
ervitaamelia <ervitaamelia@gmail.com>
2026-02-25 13:33:26 +0700

    dropdown filter select part - master data storage bin

--- module/master-data/parts.js
+++ module/master-data/parts.js
@@ -7,35 +7,38 @@ const { SParts } = db
 
 class PartsModule extends BaseModule {
   async dropdown(req) {
-  try {
-    const params = req.query || {}
-    const search = (params.search || '').trim()
-    
+    try {
+      const params = req.query || {}
 
-    const where = {}
+      const search = (params.search || '').trim()
+      const partTypeCode = (params.part_type_code || '').trim() 
 
-    if (search) {
-      where[Op.or] = [
-        { part_number: { [Op.iLike]: `%${search}%` } },
-        { part_name: { [Op.iLike]: `%${search}%` } }
-      ]
+      const where = {}
+
+      // filter part type
+      if (partTypeCode) {
+        where.part_type_code = partTypeCode
+      }
+
+      if (search) {
+        where[Op.or] = [
+          { part_number: { [Op.iLike]: `%${search}%` } },
+          { part_name: { [Op.iLike]: `%${search}%` } }
+        ]
+      }
+
+      const rows = await SParts.findAll({
+        where,
+        attributes: ['id', 'part_number', 'part_name', 'part_type_code'],
+        order: [['part_number', 'ASC']]
+      })
+
+      return { status: true, data: rows }
+    } catch (error) {
+      if (config.debug) return { status: false, error: error.message, code: 500 }
+      return { status: false, message: 'Internal server error', code: 500 }
     }
-
-    const rows = await SParts.findAll({
-      where,
-      attributes: ['id', 'part_number', 'part_name'],
-      order: [['part_number', 'ASC']]
-    })
-
-    return { status: true, data: rows }
-
-  } catch (error) {
-    if (config.debug) {
-      return { status: false, error: error.message, code: 500 }
-    }
-    return { status: false, message: 'Internal server error', code: 500 }
   }
-}
 }
 
 export default new PartsModule()

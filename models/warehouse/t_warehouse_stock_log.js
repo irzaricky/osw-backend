da5739f9f473d92e4d16ea62cea147f4fe8dfc8d
ervitaamelia <ervitaamelia@gmail.com>
2026-04-15 14:21:10 +0700

    feat: models database warehouse

---
+++ models/warehouse/t_warehouse_stock_log.js
@@ -0,0 +1,47 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+  class TWarehouseStockLog extends Model {
+    static associate(models) {
+      TWarehouseStockLog.belongsTo(models.TWarehouseStock, {
+        foreignKey: 'wh_stock_id',
+        as: 'warehouse_stock'
+      });
+
+      TWarehouseStockLog.belongsTo(models.SUsers, {
+        foreignKey: 'user_id',
+        as: 'user'
+      });
+    }
+  }
+
+  TWarehouseStockLog.init({
+    wh_stock_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    is_placement: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    qty_per_kanban: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    }
+  }, {
+    sequelize,
+    modelName: 'TWarehouseStockLog',
+    tableName: 't_warehouse_stock_log',
+    underscored: true,
+    timestamps: true,
+    paranoid: true,
+    deletedAt: 'deleted_at'
+  });
+
+  return TWarehouseStockLog;
+};

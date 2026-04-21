1b9393ddbb66fd0374de1cfa411d4803d02ff5bc
fevy <fevylestari67@gmail.com>
2026-04-21 09:45:53 +0700

    feat: work order storing

--- routes/warehouse/work-order-storing.js
+++ routes/warehouse/work-order-storing.js
@@ -5,10 +5,34 @@ import helper from '../../class/helper.class.js';
 
 const router = express.Router();
 
+// get dropdown work order storing type
+
 // get list work order storing
 router.get('/', auth.sessionChecker, async (req, res) => {
     const result = await workOrderStoringModule.list(req);
     helper.sendResponse(res, result);
 });
+
+// post add work order storing
+router.post('/', auth.sessionChecker, auth.permissionChecker(['Superadmin', 'Admin']), async (req, res) => {
+    const result = await workOrderStoringModule.add(req);
+    helper.sendResponse(res, result);
+});
+
+// get detail work order storing
+
+// put update work order storing
+router.put('/:id', auth.sessionChecker, auth.permissionChecker(['Superadmin', 'Admin']), async (req, res) => {
+    const result = await workOrderStoringModule.update(req);
+    helper.sendResponse(res, result);
+});
+
+// delete work order storing
+router.delete('/:id', auth.sessionChecker, auth.permissionChecker(['Superadmin', 'Admin']), async (req, res) => {
+    const result = await workOrderStoringModule.delete(req);
+    helper.sendResponse(res, result);
+});
+
+// print part label
 
 export default router;

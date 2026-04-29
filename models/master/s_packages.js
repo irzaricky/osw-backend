f8f9f2c02a9050875328bedd5bc699b5aeee31a9
fevy <fevylestari67@gmail.com>
2026-04-29 08:16:02 +0700

    feat: add package id to parts

--- models/master/s_packages.js
+++ models/master/s_packages.js
@@ -4,6 +4,7 @@ export default (sequelize) => {
   class SPackages extends Model {
     static associate(models) {
       SPackages.belongsTo(models.RefPackageTypes, { foreignKey: 'package_type_id', as: 'package_type' });
+      SPackages.hasMany(models.SParts, { foreignKey: 'package_id', as: 'parts' });
     }
   }
 
--- models/master/s_parts.js
+++ models/master/s_parts.js
@@ -14,6 +14,9 @@ export default (sequelize) => {
 
       // Part belongs to Supplier
       SParts.belongsTo(models.SSuppliers, { foreignKey: 'supplier_id', as: 'supplier' });
+
+      // Part belongs to Package
+      SParts.belongsTo(models.SPackages, { foreignKey: 'package_id', as: 'package' });
     }
   }
 
@@ -49,7 +52,11 @@ export default (sequelize) => {
     
     // Common
     package_name: DataTypes.STRING,
-    package_code: DataTypes.STRING
+    package_code: DataTypes.STRING,
+    package_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
   }, {
     sequelize,
     modelName: 'SParts',
